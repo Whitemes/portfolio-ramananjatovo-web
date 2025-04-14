5273d6359a1c18afa453c5bd414ba98bf68990ca
Johnny RAMANANJATOVO <ramananmichel@gmail.com>
2025-04-14 08:53:31 +0200

    Refonte de site complet

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-dark-bg text-gray-400 text-sm text-center py-6">
-        © {new Date().getFullYear()} Johnny RAMANANJATOVO — Tous droits réservés.
-      </footer>
-    );
-  }
-  
